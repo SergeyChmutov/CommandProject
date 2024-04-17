61a72cf9cb4cdf74020c317e1b2f8fc771140406
ovchinnikova <ov4innikova.e.a@mail.ru>
2024-04-17 15:06:57 +0300

    Добавлена модель, интерфес, репозиторий, сервисы для класса приюта

---
+++ src/main/java/pro/dev/animalshelter/interfaces/ShelterService.java
@@ -0,0 +1,20 @@
+package pro.dev.animalshelter.interfaces;
+
+import pro.dev.animalshelter.model.Shelter;
+import pro.dev.animalshelter.model.Users;
+
+import java.util.List;
+
+public interface ShelterService {
+    Shelter addShelter(Shelter shelter);
+
+    List<Shelter> getShelters();
+
+    Shelter updateShelter(Long id, Shelter shelter);
+
+    void deleteShelter(Long id);
+
+    List<Users> getVolunteers(Long id);
+
+//    List<Animal> getAnimals(Long id);
+}
